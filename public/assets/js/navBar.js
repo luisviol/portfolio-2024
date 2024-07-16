f843b66af7ae1d77138c13b5bac403cfe8686e79
Luis Villasmil <luisviol22@gmail.com>
2024-07-15 20:05:35 -0400

    About me + Fixed styles

--- public/assets/js/navBar.js
+++ public/assets/js/navBar.js
@@ -13,8 +13,8 @@ class NavComponent extends HTMLElement {
          </a>
          <div class="nav-items">
            <ul>
-             <li><a href="#home">Home</a></li>
-             <li><a href="#about">About</a></li>
+             <li><a href="index.html">Home</a></li>
+             <li><a href="about_me.html">About</a></li>
              <li><a href="#resume">Resume</a></li>
            </ul>
            <div class="mode-switch">
